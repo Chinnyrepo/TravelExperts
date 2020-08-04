102233f1e268866a0d20b21f2879e6660df592a6
eggs2020 <e.gs@usask.ca>
2020-08-04 16:45:41 -0600

    Added codes to packages and app.js for the packages

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 //import modules
+const port = 8000;
 const path = require("path");
 const express = require("express");
+const app = express();
 let MongoClient = require('mongodb').MongoClient;
 let url = "mongodb://localhost:27017/";
+var autoIncrement = require("mongodb-autoincrement");
 const bodyParser = require("body-parser");
 const data = require('./models/datamongo');
-
-
-//create express app
-const app = express();
 
 module.exports = app;
 
@@ -16,12 +15,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-//Serving Static files in Express
-app.use(express.static("public", 
-{
-    extensions: ["html", "css", "js", "jpg" , "png", "gif"]
-}));
 
 // automatically check if requested file is found in /public
 // if yes, return that file as a response to the browser
@@ -56,14 +49,42 @@ app.get('/contactdata', function (req, res, next) {
   })
 });
 
+/* Serving request for list of packages from packages.html
+** Edwin GonoSantosa*/
+app.get('/packages', (req, res) => {
+  //Query PACKAGES collection from TRAVELEXPERTS database
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    const dbo = db.db("travelexperts");
+    //Retrieve all documents in the collection where end date > today's date
+    dbo.collection("packages").find({"PkgEndDate" : { $gte : new Date() } }).toArray(function(err, result) {
+      if (err) throw err;
+      db.close()
+      //console.log(result);
+      //Send retrieved data from collection to the html page
+      if (result.length){
+        res.send(result);
+      }
+      else {
+          res.send('No Data');      
+      }
+    });
+  }); 
+});
+
+//Serving static files in Express
+app.use(express.static("public", {
+    extensions: ["html", "css", "js", "jpg" , "jpeg", "png", "gif"]
+}));
+
 //Endpoint handler for error page
 app.get("*", (req, res) => {
-    // /to render error page
-    res.status(404).render("Error");
+  // /to render error page
+  res.status(404).send("<h1>404: Sorry can't find that!</h1>");
 });
 
 //Listening function on port 8000
 app.listen(8000, (err) => {
-    if (err) throw err;
-      console.log("Server is listening on port 8000. Ready to accept requests.");
+  if (err) throw err;
+  console.log(`Server is listening on port ${port}`);
 });

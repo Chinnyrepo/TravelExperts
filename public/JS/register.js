cad74c5af380bc43d41d47374b0066135134edd0
eggs2020 <e.gs@usask.ca>
2020-08-04 18:56:08 -0600

    Added skyblue to registration form fields and re-arranged country and postal code fields

--- public/JS/register.js
+++ public/JS/register.js
@@ -1,36 +1,102 @@
 function myFunction(element, display) {
-    element.previousElementSibling.style.display = display;
+  element.previousElementSibling.style.display = display;
+}
+
+// Disable form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Get the forms we want to add validation styles to
+    let forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    let validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+// Function for postal code validation
+function validate(form){
+  let postalCode = document.getElementById("inputpo");
+  console.log(postalCode);
+  let reg = new RegExp(/[A-Za-z][0-9][A-Za-z]\s[0-9][A-Za-z][0-9]/);
+  console.log(reg.test(postalCode));
+  if(reg.test(postalCode.value) == false){
+    event.preventDefault();
+    alert('Invalid postal code.');
+    postalCode.focus();
+    return false;
   }
+}
 
-  // Disable form submissions if there are invalid fields
-  (function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Get the forms we want to add validation styles to
-      let forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      let validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+// Function for adding sky-blue backgroudn color to the input fields
+// Oyakhire Airende
+function BlueFunction(x){
+  x.style.background = "skyblue";
+}
 
-   // Function for form validation
-    function validate(form){
-      let postalCode = document.getElementById("inputpo");
-      console.log(postalCode);
-      let reg = new RegExp(/[A-Za-z][1-9][A-Za-z]\s[1-9][A-Za-z][1-9]/);
-      console.log(reg.test(postalCode));
-      if(reg.test(postalCode.value) == false){
-        event.preventDefault();
-        alert('Invalid postal code.');
-        postalCode.focus();
-        return false;
-      }
-    }
+// Function for validating other form input fields
+// Oyakhire Airende
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+ 
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function () {
+  document.getElementById("message").style.display = "block";
+}
+ 
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function () {
+  document.getElementById("message").style.display = "none";
+}
+ 
+// When the user starts to type something inside the password field
+myInput.onkeyup = function () {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if (myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+ 
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if (myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+ 
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if (myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+ 
+  // Validate length
+  if (myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
